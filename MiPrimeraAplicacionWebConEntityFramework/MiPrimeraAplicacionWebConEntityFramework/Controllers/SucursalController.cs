c70b6fd150e18608d3d087959bf0342ea785dba8
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2020-09-26 22:36:48 -0500

    practica con el tag datatime

--- MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Controllers/SucursalController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Controllers/SucursalController.cs
@@ -16,16 +16,48 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
             using(var bd=new BDPasajeEntities())
             {
                 listaSucursales = (from item in bd.Sucursal
-                                   where item.BHABILITADO==1
-                                  select new SucursalCLS
-                                  {
-                                      IDSucursal = item.IIDSUCURSAL,
-                                      Nombre = item.NOMBRE,
-                                      Telefono = item.TELEFONO,
-                                      Email = item.EMAIL
-                                  }).ToList();
+                                   where item.BHABILITADO == 1
+                                   select new SucursalCLS
+                                   {
+                                       IDSucursal = item.IIDSUCURSAL,
+                                       Nombre = item.NOMBRE,
+                                       Direccion = item.DIRECCION,
+                                       Telefono = item.TELEFONO,
+                                       Email = item.EMAIL,
+                                       FechaApertura = item.FECHAAPERTURA.Value//se debe colocar value y el valor no puede ser nulo en la bd
+                                   }).ToList();
             }
             return View(listaSucursales);
+        }
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(SucursalCLS osucursalCLS) //nos ingresa los datos via post
+        {
+            if (ModelState.IsValid)
+            {
+                using (var bd = new BDPasajeEntities())
+                {
+                    Sucursal osucursal = new Sucursal(); //clase de entity framework
+                    osucursal.NOMBRE = osucursalCLS.Nombre;
+                    osucursal.DIRECCION = osucursalCLS.Direccion;
+                    osucursal.TELEFONO = osucursalCLS.Telefono;
+                    osucursal.EMAIL = osucursalCLS.Email;
+                    osucursal.FECHAAPERTURA = osucursalCLS.FechaApertura;
+                    osucursal.BHABILITADO = 1;
+                    bd.Sucursal.Add(osucursal);
+                    bd.SaveChanges();
+                }
+            }
+            else
+            {
+                return View(osucursalCLS);//para que la informacion se mantenga y no se borre
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Models/SucursalCLS.cs
+++ MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Models/SucursalCLS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,35 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Models
 {
     public class SucursalCLS
     {
+        [Display(Name ="Id sucursal")]
+        [Required]
         public int IDSucursal { get; set; }
+
+        [Display(Name = "Nombre sucursal")]
+        [StringLength(100, ErrorMessage ="Longitud maxima 100")]
+        [Required]
         public string Nombre { get; set; }
+
+        [Display(Name = "Direccion sucursal")]
+        [StringLength(200, ErrorMessage = "Longitud maxima 200")]
+        [Required]
         public string Direccion { get; set; }
+
+        [Display(Name = "Telefono sucursal")]
+        [StringLength(10, ErrorMessage = "Longitud maxima 10")]
+        [Required]
         public string Telefono { get; set; }
+
+        [Display(Name = "Email sucursal")]
+        [StringLength(100, ErrorMessage = "Longitud maxima 100")]
+        [EmailAddress(ErrorMessage ="Ingrese un email valido")]
+        [Required]
         public string Email { get; set; }
+
+        [Display(Name = "FechaApertura sucursal")]
+        [Required]
+        [DataType(DataType.Date)]//AYUDA EN LA VISTA
+        [DisplayFormat(DataFormatString ="{0:YYYY-MM-DD}", ApplyFormatInEditMode =true)]//DA FORMATO
         public DateTime FechaApertura { get; set; }
         public int BHabilitado { get; set; }
     }
