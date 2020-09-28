84390bf9a780e02e0fa7a3f42e57f3c90f4f3ef9
jaardila-3 <jorge.ardila1641@correo.policia.gov.co>
2020-09-27 23:17:11 -0500

    se agrego una lista para crear el combobox de sexo

--- MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Controllers/ClienteController.cs
+++ MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Controllers/ClienteController.cs
@@ -23,10 +23,37 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Controllers
                                        Nombre = item.NOMBRE,
                                        APPaterno = item.APPATERNO,
                                        APMaterno = item.APMATERNO,
-                                       Direccion = item.DIRECCION
+                                       Direccion = item.DIRECCION,
+                                       IDSexo = item.IIDSEXO.Value //agregar value
                                    }).ToList();
             }
             return View(listaClientes);
+        }
+
+        //creamos el combobox para sexo con una lista
+        List<SelectListItem> listaSexo;
+        private void LlenarSexo()
+        {
+            using (var bd=new BDPasajeEntities())
+            {
+                listaSexo = (from item in bd.Sexo
+                             where item.BHABILITADO == 1
+                             select new SelectListItem // las listas SelectListItem tienen dos propiedades que son
+                             {
+                                 Text = item.NOMBRE,//Text que es lo que el usuario va a escoger
+                                 Value = item.IIDSEXO.ToString()//Value que es su valor interno o asignado
+                             }).ToList();
+                //para agregar "seleccione" en la primera posicion=0 de la lista se hace lo sgte:
+                listaSexo.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+            }
+        }
+
+        public ActionResult Agregar()
+        {
+            LlenarSexo();
+            //con ViewBag puedes pasar info a la vista, despues de ViewBag puedes agregar lo que quiera, en este caso lista
+            ViewBag.lista = listaSexo;//permite pasar la lista a la vista
+            return View();
         }
     }
 }
--- MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Models/ClienteCLS.cs
+++ MiPrimeraAplicacionWebConEntityFramework/MiPrimeraAplicacionWebConEntityFramework/Models/ClienteCLS.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,33 @@ namespace MiPrimeraAplicacionWebConEntityFramework.Models
 {
     public class ClienteCLS
     {
+        [Display(Name ="Id Cliente")]
         public int IDCliente { get; set; }
+
+        [Display(Name = "Nombre Cliente")]
+        [StringLength(100,ErrorMessage ="Longitud maxima 100")]
+        [Required]
         public string Nombre { get; set; }
+
+        [Display(Name = "Apellido Paterno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
+        [Required]
         public string APPaterno { get; set; }
+
+        [Display(Name = "Apellido Materno")]
+        [StringLength(150, ErrorMessage = "Longitud maxima 150")]
+        [Required]
         public string APMaterno { get; set; }
+
+        [Display(Name = "Direccion Cliente")]
+        [StringLength(200, ErrorMessage = "Longitud maxima 200")]
+        [Required]
         public string Direccion { get; set; }
+
+        [Display(Name = "Sexo Cliente")]
+        [Required]
+        public int IDSexo { get; set; }
+
         public int BHabilitado { get; set; }
     }
 }
